57d9680b4d0442a6c3a212a3145043e3aca8f652
ErfanAbedinpour <erfan.wtf44@gmail.com>
2024-06-16 13:33:36 -0500

    deleted some files we dont needit not

--- modules/v1/mail/mail.validator.js
+++
@@ -1,7 +0,0 @@
-const yup = require("yup");
-
-exports.sendMail = yup.object({
-  userName: yup.string().required("userName is required"),
-  text: yup.string().required("please enter your text"),
-  email: yup.string().email("enter valid email").required("email is required"),
-});
